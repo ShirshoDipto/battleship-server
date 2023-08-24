4a95353758270cbba9e506aaa71d893bea3e37d1
ShirshoDipto <shirshodipto@gmail.com>
2023-08-24 15:02:01 +0600

    new implementation for deleting a room

--- index.js
+++ index.js
@@ -81,7 +81,10 @@ io.on("connection", (socket) => {
 
   socket.on("leaveRoom", ({ roomId, isGameOver }) => {
     if (roomId && rooms.has(roomId)) {
-      rooms.delete(roomId);
+      rooms.get(roomId).forEach((socketId) => {
+        io.sockets.sockets.get(socketId).leave(roomId);
+      });
+
       if (!isGameOver) {
         socket.to(roomId).emit("error", "Opponent has left the game. ");
       }
